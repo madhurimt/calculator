83c307ace399fb4ccdfc8bef5e6eaffe018264bf
madhurimt <madhuri.maniktahla@gmail.com>
2022-10-04 17:25:54 +1100

    adding calculating functionality

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const buttons = document.querySelectorAll("button");
-const screen = document.getElementById("calculation_screen");
+let string = "";
 
 buttons.forEach(function (btn) {
   btn.addEventListener("click", (e) => {
     const button_clicked = e.currentTarget.textContent;
-    console.log(button_clicked);
+    if (button_clicked == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (button_clicked == "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else {
+      string = string + button_clicked;
+      document.querySelector("input").value = string;
+    }
   });
 });
