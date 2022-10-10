f2f03a626d4df05e6bf2a7c75c60064c42d10652
madhurimt <madhuri.maniktahla@gmail.com>
2022-10-10 14:46:47 +1100

    fixing UI and resetting the calc after = click

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 const buttons = document.querySelectorAll("button");
-let string = "";
+let user_input = "";
 let calc_done = false;
 
 buttons.forEach(function (btn) {
   btn.addEventListener("click", (e) => {
     const button_clicked = e.currentTarget.textContent;
-    if (calc_done) {
-      string = "";
-      document.querySelector("input").value = string;
+    if (calc_done && button_clicked !== "C") {
+      user_input = button_clicked;
+      document.querySelector("input").value = user_input;
       calc_done = false;
     } else if (button_clicked == "=") {
-      if (string !== "") {
-        string = eval(string);
-        document.querySelector("input").value = string;
+      if (user_input !== "") {
+        let answer = eval(user_input);
+        document.querySelector("input").value = answer;
         calc_done = true;
       }
     } else if (button_clicked == "C") {
-      string = "";
-      document.querySelector("input").value = string;
+      user_input = "";
+      document.querySelector("input").value = user_input;
     } else {
-      string = string + button_clicked;
-      document.querySelector("input").value = string;
+      user_input += button_clicked;
+      console.log(user_input);
+      document.querySelector("input").value = user_input;
     }
   });
 });
