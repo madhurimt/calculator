3d01e5a927021827d96238253afa3586b37f1efa
madhurimt <madhuri.maniktahla@gmail.com>
2022-10-10 11:56:07 +1100

    reseting calc after = click

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const buttons = document.querySelectorAll("button");
 let string = "";
+let calc_done = false;
 
 buttons.forEach(function (btn) {
   btn.addEventListener("click", (e) => {
     const button_clicked = e.currentTarget.textContent;
-    if (button_clicked == "=") {
-      string = eval(string);
+    if (calc_done) {
+      string = "";
       document.querySelector("input").value = string;
+      calc_done = false;
+    } else if (button_clicked == "=") {
+      if (string !== "") {
+        string = eval(string);
+        document.querySelector("input").value = string;
+        calc_done = true;
+      }
     } else if (button_clicked == "C") {
       string = "";
       document.querySelector("input").value = string;
